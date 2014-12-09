9a47693119b1ceab0834c485dd11654af0d5cbbb
Alex Hubers <ahubers15@cornellcollege.edu>
2014-12-08 22:45:49 -0600

    trying to 1. add start variable in search query, and 2. Make sure image actually loads.

--- SwayzeBaby.py
+++ SwayzeBaby.py
@@ -6,6 +6,7 @@
 import urllib2
 # import re
 import random
+import requests
 
 import json
 import pprint
@@ -13,19 +14,25 @@ import pprint
 
 class SwayzeBaby:
 
-	makeSomeBabies = ['Road%20House', "Ghost", 'dance', 'point%20Break', 'havana%20nights','actor','dirty%20Dancing','celebrity','image','muscles']
+	Swaykeys = ['Road%20House', "Ghost", 'dance', 'point%20Break', 'havana%20nights','dirty%20Dancing']
 
 	def __init__(self, image=None):
 		if (image):
 			self.image = image
 		else:
 			self.image = self.makeSomeSwayze()
+			while True:
+				try:
+					image_r = requests.get(self.image)
+					break
+				except ConnectionError, e:
+					self.image = self.makeSomeSwayze()
 	
 	# Returns an image of Swayze the man Swayze baby.
 	def makeSomeSwayze(self):
 		#I do not believe Google is too damn keen on me actually performing a google image search.
 		#To top that, if you want ot use their API you are limited to 100 calls per day. This method only returns a very short list of
-		#the Swayze. But hell, nobody puts baby in the corner... I surely can still get random pictures with sufficiently varied queries!
+		#the Swayze. But nobody puts baby in the corner... I surely can still get random pictures with sufficiently varied queries!
 		swayzeUrl = self.__nobodyPutsBabyInTheCorner()
 			
 		data = json.load(urllib2.urlopen(swayzeUrl))
@@ -36,9 +43,9 @@ class SwayzeBaby:
 	#Generates a random Swayze related search query.
 	def __nobodyPutsBabyInTheCorner(self):
 		swayzeUrl = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=PATRICK%20SWAYZE";
-		howManyBabies = random.randint(0,len(self.makeSomeBabies))
-		for i in range(1, howManyBabies):
-			swayzeUrl += "%20"  + random.choice(self.makeSomeBabies)
+		swayzeUrl += "%20"  + random.choice(self.swaykeys)
+		start = random.randint(1,30)
+		swayzeUrl += "&start={0}".format(start)
 		return swayzeUrl
 
 	#Since formatting is hard in html + python.
