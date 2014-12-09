c8dd1991c0f286f5ba624c6ba670ce709e9b50ee
Alex Hubers <ahubers15@cornellcollege.edu>
2014-12-08 21:21:35 -0600

    Trying to avoid google's query API call block by just having sufficiently random search queries.

--- SwayzeBaby.py
+++ SwayzeBaby.py
@@ -5,60 +5,42 @@
 # import cv2
 import urllib2
 # import re
-# import random
+import random
 
 import json
 import pprint
 
 
 class SwayzeBaby:
+
+	makeSomeBabies = ['Road%20House', "Ghost", 'dance', 'point%20Break', 'havana%20nights','actor','dirty%20Dancing','celebrity','image','muscles']
+
 	def __init__(self, image=None):
 		if (image):
 			self.image = image
 		else:
 			self.image = self.makeSomeSwayze()
-
+	
+	# Returns an image of Swayze the man Swayze baby.
 	def makeSomeSwayze(self):
-		data = json.load(urllib2.urlopen('https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=PATRICK%20SWAYZE'))
+		#I do not believe Google is too damn keen on me actually performing a google image search.
+		#To top that, if you want ot use their API you are limited to 100 calls per day. This method only returns a very short list of
+		#the Swayze. But hell, nobody puts baby in the corner... I surely can still get random pictures with sufficiently varied queries!
+		swayzeUrl = self.__nobodyPutsBabyInTheCorner()
+			
+		data = json.load(urllib2.urlopen(SwayzeUrl))
+		print json.dumps(jsonStr, sort_keys=True, indent=2, separators=(',', ': '))
 
-		pprint.pprint(data)
+	#Generates a random Swayze related search query.
+	def __nobodyPutsBabyInTheCorner(self):
+		swayzeUrl = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=PATRICK%20SWAYZE";
+		howManyBabies = random.randInt(0,len(makeSomeBabies))
+		for i in range(1, howManyBabies):
+			swayzeUrl += "%20"  + random.choice(makeSomeBabies)
+		return swayzeUrl
 
 
-		# #Stolen from http://www.morwire.com/index.php/python-tutorials/4-scraping-images-from-google
-		# url = "https://www.google.ca/search?site=&tbm=isch&q=Patrick+Swayze#q=Patrick+Swayze&tbm=isch"
-		# user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-		# #we need to put the user agent into a header format..
-		# headers = {'User-Agent':user_agent,}
-		  
-		# #we also need to put it into the form of a request for urllib2
-		# search_request = urllib2.Request(url,None,headers)
-		  
-		# #now we can request and print the data
-		# search_results = urllib2.urlopen(search_request)
-		# search_data = search_results.read()
-		  
-		# #Now we need to parse the data for links
-		# #our first step is to compile a regular expression
-		# pattern = re.compile('imgurl=([^>]+)&amp;imgrefurl')
-		# #now we can search through the data with the new RE
-		# image_list = pattern.findall(search_data)
-		  
-		  
-		# #Its time to select a random link until we get a valid image
-		# while True:
-		# 	#choose a random number..
-		# 	index = random.randint(0,len(image_list)-1)
-		# 	#create the request for urllib2
-		# 	image_request = urllib2.Request(image_list[index],None,headers)
-		# 	#make sure its a valid link..
-		# 	try:
-		# 		image_remote = urllib2.urlopen(image_request)
-		# 		if image_remote.headers.maintype == 'image':
-		# 			break
-		# 	except:
-		# 		#this is here just so that we have something..
-		# 		index = 0
-		# print "<p><img src='" + image_list[index] + "''></p>"
 
+	#Turns self.image into a histogram to generate. 
 	def getColorScheme(self):
 		return ("#000000", "#111111", "#FFFFFF", "#123456")
