eded9cdc1c260bcd44a201ac366a52a9c8796e11
Alex Hubers <ahubers15@cornellcollege.edu>
2015-01-31 15:56:20 -0600

    cleaning up collectColors.py

--- collectColors.py
+++ collectColors.py
@@ -10,11 +10,9 @@ selector.query("SELECT id, url FROM swayze where fake=0")
 for row in selector.cur:
    swayze_id = row[0]
    url = row[1]
-   print url
    colors = swayze.getColorScheme(url)
    for color in colors:
-	   query = "INSERT INTO colors (swayze_id, color, fake) VALUES ({0}, '{1}', {2})".format(swayze_id, color, 0)
-	   insertor.query(query)
-				
+	   query = "INSERT INTO colors (swayze_id, color) VALUES (%s, %s)"
+	   insertor.cur.execute(query, (swayze_id, color) )				
 				
 
