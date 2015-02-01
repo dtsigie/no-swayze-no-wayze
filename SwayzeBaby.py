5204cabe8b95e7c1d0dadca7e3a348987fa43fc9
Alex Hubers <ahubers15@cornellcollege.edu>
2015-02-01 15:03:53 -0600

    Modifying for use on the website.

--- SwayzeBaby.py
+++ SwayzeBaby.py
@@ -25,56 +25,39 @@ class SwayzeBaby:
 
 	def __init__(self, image=None):
 		return
-		# if (image):
-		# 	self.image = image
-		# else:
-		# 	self.image = self.makeSomeSwayze()
-		# 	while True:
-		# 		try:
-		# 			image_r = urllib2.urlopen(self.image)
-		# 			break
-		# 		except:
-		# 			self.image = self.makeSomeSwayze()
-	
-	# Returns an image of Swayze the man Swayze baby.
-	def makeSomeSwayze(self):
-		#I do not believe Google is too damn keen on me actually performing a google image search.
-		#To top that, if you want ot use their API you are limited to 100 calls per day. This method only returns a very short list of
-		#the Swayze. But nobody puts baby in the corner... I surely can still get random pictures with sufficiently varied queries!
-		swayzeUrl = self.__nobodyPutsBabyInTheCorner()
-			
-		data = json.load(urllib2.urlopen(swayzeUrl))
-		#self.printItems(data, 0)
 
-		luckySwayze = random.randint(0, 3)
-		return data['responseData']['results'][luckySwayze]['url']
-	#Generates a random Swayze related search query.
-	def __nobodyPutsBabyInTheCorner(self):
-		swayzeUrl = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=PATRICK%20SWAYZE";
-		swayzeUrl += "%20"  + random.choice(self.swaykeys)
-		start = random.randint(1,30)
-		swayzeUrl += "&start={0}".format(start)
-		return swayzeUrl
+	def getRandSwayzeColors(self):
+		query = """
+		SELECT id, url FROM table WHERE fake=0
+		ORDER BY RAND()
+		LIMIT 1
+		"""
+		corner = Corner()
+		corner.cur.execute(query)
 
-	#Since formatting is hard in html + python.
-	# def printItems(self, dictObj, indent=" "):
-	# 	print '  '*indent + '<ul>\n'
-	# 	for k,v in dictObj.iteritems():
-	# 		if isinstance(v, dict):
-	# 			print '  '*indent , '<li>', k, ':', '</li>'
-	# 			self.printItems(v, indent+1)
-	# 		else:
-	# 			if isinstance(v, list):
-	# 				i = 0
-	# 				print '  '*indent , '<li>', k, ':', '</li>'
-	# 				for item in v:
-	# 					print '  '*indent , '<li>', i, ':', '</li>'
-	# 					i += 1
-	# 					self.printItems(item, indent+1)
-	# 					print '</br></br>'
-	# 			else:
-	# 				print ' '*indent , '<li>', k, ':', v, '</li>'
-	# 	print '  '*indent + '</ul>\n'
+		for row in corner.cur:
+			swayze_id = row[0]
+			url = row[1]
+
+		colors = []
+		colors.append(url)
+		corner.cur.execute("SELECT color FROM colors WHERE swayze_id = " + swayze_id)
+		for row in corner.cur:
+			colors.append(row[0])
+		return colors
+
+
+
+	def getDuplicateSwayze(self):
+		query = """
+		  select url, count(*)
+		  from swayze
+		  group by url
+		  having count(*) > 1
+		"""
+
+		corner = Corner()
+		corner.cur.execute(query)
 
 	#Turns self.image into a histogram to generate. 
 	def getColorScheme(self, url):
--- index.py
+++ index.py
@@ -11,18 +11,16 @@ roadHouse = SwayzeBaby()
 
 print "<head>"
 print "<title>No Swayze No Wayze</title>"
-print '<link rel="stylesheet" type="text/css" href="css.css">'
-print roadHouse.CSSwayze()
+# print '<link rel="stylesheet" type="text/css" href="css.css">'
+# print roadHouse.CSSwayze()
 print "</head>"
 
 #Body stuff.
 print "<body>"
-print """
-	<header>
-	</header>
+print "<div id='container'>"
+colors = roadHouse.getRandSwayzeColors()
+	# print """<img src='{0}' align='middle'></img> """.format(roadHouse.image)
+	#Put swayze in center, baby.
 
-"""
-
-print """<img src='{0}' align='middle'></img> """.format(roadHouse.image)
-#Put swayze in center, baby.
+print "</div>"
 print "</body>"
