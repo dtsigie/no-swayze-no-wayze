8d7d9c75fde584fb2ebbbfe29d79b9974f0c9881
Alex Hubers <ahubers15@cornellcollege.edu>
2015-08-29 12:02:30 -0500

    Use crappier way of hiding config. Python does not support env variables very easily.

--- core/corner.py
+++ core/corner.py
@@ -1,13 +1,10 @@
 import pymysql
-import os
+import envVariables
+
 #Wrapper for SQL.
 class Corner:
         cur = ""
-        DATABASE_USER = os.environ['DATABASE_USER']
-        DATABASE_HOST = os.environ['DATABASE_HOST']
-        DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
-        DATABASE_NAME = os.environ['DATABASE_NAME']
-        
+                
         def __init__(self):
                 self.conn = pymysql.connect(user=DATABASE_USER,port=3306, passwd=DATABASE_PASSWORD, host=DATABASE_HOST, db=DATABASE_NAME)
                 self.cur = self.conn.cursor()
